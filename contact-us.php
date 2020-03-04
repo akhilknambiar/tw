0ead41a775b77532e3d26c8f066c0a161b9952e1
Akhil K Nambiar <akhilknambiar@gmail.com>
2020-03-04 23:34:09 +0530

    Changed to common header

--- contact-us.php
+++ contact-us.php
@@ -47,72 +47,7 @@
 </div>
 <!-- Page loader End -->
 
-<header class="fixed-top header-fullpage top-border top-transparent wow fadeInDown">
-    <div class="top-bar-right d-flex align-items-center text-md-left">
-        <div class="container">
-            <div class="row align-items-center p-2">
-                <div class="col">
-                    <i class="icofont-google-"></i>Office No.30, Nasser Lootah Airport Building, Near Dubai Cargo
-                </div>
-                <div class="col-md-auto">
-
-                    <span class="mr-3"><i class="icofont-ui-touch-phone"></i> <a href="tel:+971 4 2087614">+971 4 2087614</a></span>
-                    <div class="clearfix"></div>
-                    <span class="mr-3"><a href="mailto:sales@truewayslogistics.com"><i class="icofont-ui-email"></i> sales@truewayslogistics.com</a></span>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Main Navigation Start -->
-    <nav class="navbar navbar-expand-lg bg-transparent">
-        <div class="container text-nowrap">
-            <div class="d-flex align-items-center w-100 col p-0">
-                <a class="navbar-brand rounded-bottom light-bg" href="index.php">
-                    <img alt="" src="images/logo_footer.png">
-                </a>
-            </div>
-            <!-- Topbar Request Quote Start -->
-            <div class="d-inline-flex request-btn order-lg-last col p-0">
-                <a class="btn-theme icon-left bg-navy-blue no-shadow align-self-center" data-target="#request_popup"
-                   data-toggle="modal"
-                   href="#" role="button"><i class="icofont-list"></i> <span
-                            class="d-none d-sm-inline-block"> Request Quote</span></a>
-                <!-- Toggle Button Start -->
-                <button aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"
-                        class="navbar-toggler x collapsed" data-target="#navbarCollapse" data-toggle="collapse"
-                        type="button">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <!-- Toggle Button End -->
-            </div>
-            <!-- Topbar Request Quote End -->
-
-            <div class="collapse navbar-collapse" data-animations="slideInUp slideInUp slideInUp slideInUp"
-                 data-hover="dropdown"
-                 id="navbarCollapse">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about-us.php">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="services.php">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact-us.php">Contact Us</a>
-                    </li>
-                </ul>
-                <!-- Main Navigation End -->
-            </div>
-        </div>
-    </nav>
-    <!-- Main Navigation End -->
-</header>
+<?php include('header.php') ?>
 
 <!-- Page Breadcrumbs Start -->
 <div class="slider bg-navy-blue bg-scroll pos-rel breadcrumbs-page">
