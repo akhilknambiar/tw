a20f81d0c2ba1614763caad4f1c4f126ec87c2d8
Akhil K Nambiar <akhilknambiar@gmail.com>
2020-03-04 23:27:43 +0530

    Moved header to common file

--- services.php
+++ services.php
@@ -29,82 +29,23 @@
 <body>
 
   <!-- Page loader Start -->
-  <div id="pageloader">   
-    <div class="loader-item">
-      <div class="loader">
-        <div class="spin"></div>
-          <div class="bounce"></div>
+  <div id="pageloader">
+      <div class="loader-item">
+          <div class="loader">
+              <div class="spin"></div>
+              <div class="bounce"></div>
+          </div>
       </div>
-    </div>
   </div>
   <!-- Page loader End -->
 
-  <header class="fixed-top header-fullpage top-border top-transparent wow fadeInDown">
-      <div class="top-bar-right d-flex align-items-center text-md-left">
-          <div class="container">
-              <div class="row align-items-center p-2">
-                  <div class="col">
-                      <i class="icofont-google-"></i>Office No.30, Nasser Lootah Airport Building, Near Dubai Cargo
-                      Village
-                  </div>
-                  <div class="col-md-auto">
-
-                      <span class="mr-3"><i class="icofont-ui-touch-phone"></i> <a href="tel:+971 4 2087614">+971 4 2087614</a></span>
-                      <div class="clearfix"></div>
-                      <span class="mr-3"><a href="mailto:sales@truewayslogistics.com"><i class="icofont-ui-email"></i> sales@truewayslogistics.com</a></span>
-                  </div>
-              </div>
-          </div>
-      </div>
-
-      <!-- Main Navigation Start -->
-      <nav class="navbar navbar-expand-lg bg-transparent">
-          <div class="container text-nowrap">
-              <div class="d-flex align-items-center w-100 col p-0">
-          <a class="navbar-brand rounded-bottom light-bg" href="index.php">
-            <img src="images/logo_footer.png" alt="">
-          </a> 
-        </div>
-        <!-- Topbar Request Quote Start -->
-        <div class="d-inline-flex request-btn order-lg-last col p-0">  
-          <a class="btn-theme icon-left bg-navy-blue no-shadow align-self-center" href="#" role="button" data-toggle="modal" data-target="#request_popup"><i class="icofont-list"></i> <span class="d-none d-sm-inline-block"> Request Quote</span></a>
-          <!-- Toggle Button Start -->
-          <button class="navbar-toggler x collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-          <!-- Toggle Button End -->  
-        </div>
-        <!-- Topbar Request Quote End -->
-
-        <div class="collapse navbar-collapse" id="navbarCollapse" data-hover="dropdown" data-animations="slideInUp slideInUp slideInUp slideInUp">
-			<ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about-us.php">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="services.php">Services</a>
-              </li>  
-              <li class="nav-item">
-                <a class="nav-link" href="contact-us.php">Contact Us</a>
-              </li>
-          </ul>
-          <!-- Main Navigation End -->
-        </div>
-      </div>
-    </nav>
-    <!-- Main Navigation End -->
-  </header>
+  <?php include('header.php') ?>
 
   <!-- Page Breadcrumbs Start -->
   <div class="slider bg-navy-blue bg-scroll pos-rel breadcrumbs-page">
-    <div class="container">
-      <h1>Our Services</h1>
-    </div>
+      <div class="container">
+          <h1>Our Services</h1>
+      </div>
   </div>
   <!-- Page Breadcrumbs End -->
 
