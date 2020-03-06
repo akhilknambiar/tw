3a5b324518c9fa36e30ff2313e5a0e32b96d8a10
Akhil K Nambiar <akhilknambiar@gmail.com>
2020-03-06 16:57:52 +0530

    Added stat counter

--- footer.php
+++ footer.php
@@ -6,7 +6,11 @@
             <div class="row text-md-left text-center">
                 <div class="col-sm-12 col-md-6 copyright-links">
                     <a href="#">TrueWays © 2020</a> - FOLLOW US: <i class="icofont-facebook"></i>
+                    <!-- Start of Simple-Counter Code -->
+                    <img src="https://www.simple-counter.com/hit.php?id=zrvvvodqf&nd=6&nc=4&bc=1" border="0" alt="Hit Counter">
+                    <!-- End of Simple-Counter Code -->
                 </div>
+
                 <div class="col-sm-12 col-md-6 text-md-right text-center">
                     Designed by <a href="#" rel="nofollow">CREATIVES@</a>
                 </div>
