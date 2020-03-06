5bb5680710c77d3b077540c90657331e77d75885
Akhil K Nambiar <akhilknambiar@gmail.com>
2020-03-06 15:56:28 +0530

    Removed Read more and Learn More

--- index.php
+++ index.php
@@ -263,32 +263,6 @@ Near Dubai Cargo Village, Dubai, U.A.E
                   <!--                First Page text here-->
               </div>
 
-              <!-- LAYER NR. 12 -->
-              <div class="tp-caption tp-resizeme btn-theme bg-navy-blue rev-btn"
-                   id="slide-82-layer-5"
-                   data-x="['center','center','center','center']"
-                   data-hoffset="['0','0','0','0']"
-                   data-y="['middle','middle','middle','middle']"
-                   data-voffset="['140','140','140','140']"
-                   data-width="none"
-                   data-height="none"
-                   data-whitespace="nowrap"
-
-                   data-type="text"
-                   data-responsive_offset="on"
-
-                   data-frames='[{"delay":750,"speed":1000,"sfx_effect":"blockfromleft","sfxcolor":"#ffffff","frame":"0","from":"z:0;","to":"o:1;","ease":"Power4.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
-                   data-textAlign="['inherit','inherit','inherit','inherit']"
-
-                   data-paddingtop="[0,0,0,0]"
-                   data-paddingright="[30,30,30,30]"
-                   data-paddingbottom="[0,0,0,0]"
-                   data-paddingleft="[30,30,30,30]"
-
-                   data-fontsize="['14','14','14','14']"
-                   data-lineheight="['16','16','16','16']"
-
-              >Learn More <i class="icofont-rounded-right"></i></div>
           </li>
             <!-- SLIDE  -->
             <li data-index="rs-3045" data-transition="zoomout" data-slotamount="default" data-hideafterloop="0"
@@ -345,29 +319,6 @@ Near Dubai Cargo Village, Dubai, U.A.E
                     <!--                slide 2 text-->
                 </div>
 
-                <!-- LAYER NR. 3 -->
-                <div class="tp-caption BigBold-Button rev-btn "
-                     id="slide-3-layer-3"
-                     data-x="left" data-hoffset="60"
-                     data-y="center" data-voffset="100"
-                     data-width="['auto']"
-                     data-height="['auto']"
-                     data-transform_idle="o:1;"
-                     data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:300;e:Power1.easeInOut;"
-                     data-style_hover="c:rgba(255, 255, 255, 1.00);bg:rgba(41, 46, 49, 0);bc:rgba(255, 255, 255, 1.00);cursor:pointer;"
-
-                     data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power3.easeInOut;"
-                     data-transform_out="s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;"
-                     data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
-                     data-start="2100"
-                     data-splitin="none"
-                     data-splitout="none"
-                     data-actions='[{"event":"click","action":"jumptoslide","slide":"next","delay":""}]'
-                data-responsive_offset="on" 
-                data-responsive="off"
-                
-                style="z-index: 3; white-space: nowrap; font-weight: 800;background-color:rgba(41, 46, 49, 1.00);border-color:rgba(255, 255, 255, 0);outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;">READ MORE
-            </div>
           </li>
         </ul>
         <div class="tp-bannertimer tp-bottom" style="height: 7px; background-color: rgba(255, 255, 255, 0.25);"></div>
