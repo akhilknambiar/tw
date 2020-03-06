53f4253b9a1502b2d070a7d82dd7a65b203b97bb
Akhil K Nambiar <akhilknambiar@gmail.com>
2020-03-06 16:48:26 +0530

    Common footer.php

--- contact-us.php
+++ contact-us.php
@@ -246,24 +246,7 @@
 </main>
 
 
-<!-- Main Footer Start -->
-<footer class="bg-sky-blue pb-0">
-
-    <div class="copyright-wrap bg-navy-blue wide-tb-30">
-        <div class="container">
-            <div class="row text-md-left text-center">
-                <div class="col-sm-12 col-md-6 copyright-links">
-                    <a href="#">TrueWays © 2020</a> - FOLLOW US: <i class="icofont-facebook"></i>
-                </div>
-                <div class="col-sm-12 col-md-6 text-md-right text-center">
-                    Designed by <a href="#" rel="nofollow">CREATIVES@</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- Main Footer End -->
-
+<?php include('footer.php')?>
 <?php include('popup.php'); ?>
 
 <!-- Back To Top Start -->
